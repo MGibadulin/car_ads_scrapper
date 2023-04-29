211d41551ecebf2c896e0f807e5a6989c9935236
timoti1 <timoti@tut.by>
2023-04-29 17:35:05 +0300

    sh (setup.sh) script for setting the working environment

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ pip3 install -r requirements.txt --no-cache-dir
 
 sudo apt install openjdk-8-jre-headless
 
-echo export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre >> .bashrc
-echo export PATH=$JAVA_HOME/bin:$PATH >> .bashrc
+echo export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre >> ~/.bashrc
+echo export PATH=$JAVA_HOME/bin:$PATH >> ~/.bashrc
 
