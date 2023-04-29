c9f715a564063a52027fa0e4cc2142d6c1c889c8
timoti1 <timoti@tut.by>
2023-04-29 17:31:56 +0300

    sh (setup.sh) script for setting the working environment

---
+++ setup.sh
@@ -0,0 +1,9 @@
+pip3 install -r requirements.txt --no-cache-dir
+
+#export SPARK_HOME=~/.local/bin/pyspark
+
+sudo apt install openjdk-8-jre-headless
+
+echo export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre >> .bashrc
+echo export PATH=$JAVA_HOME/bin:$PATH >> .bashrc
+
