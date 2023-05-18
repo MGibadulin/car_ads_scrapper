da08dd00ad9c4db2fc286b545c809c925ef9ffe3
timoti1 <timoti@tut.by>
2023-05-19 01:22:04 +0300

    fixed deployment scripts

--- deployment/setup_e2_micro_vm.sh
+++ deployment/setup_e2_micro_vm.sh
@@ -19,14 +19,14 @@ sudo docker image pull mysql/mysql-server:8.0
 sudo docker run --name mysql --restart=always -p 3306:3306 -v /mnt/disk-for-data/mysql:/var/lib/mysql/ -d -e "MYSQL_ROOT_PASSWORD=enter1" mysql/mysql-server:8.0
 
 sudo mkdir /soft
-git clone https://github.com/timoti1/car_ads_scrapper /soft/car_ads_scrapper
+sudo git clone https://github.com/timoti1/car_ads_scrapper /soft/car_ads_scrapper
 
 #sudo apt install --yes openjdk-8-jre-headless
 #export JAVA_HOME=/usr/bin/java              #$(which java)
 #echo "JAVA_HOME=/usr/bin/java" >> /etc/environment
 
-apt install --yes python3-pip
-pip3 install -r /soft/car_ads_scrapper/requirements_scrapper.txt
+sudo apt install --yes python3-pip
+sudo pip3 install -r /soft/car_ads_scrapper/requirements_scrapper.txt
 
 # set the rdbms up
 sudo docker exec -i mysql bash  <<< "echo 'bind-address  = 0.0.0.0' >> /etc/my.cnf"
@@ -36,13 +36,17 @@ sudo docker restart mysql
 
 sudo apt install --yes mysql-client
 
-
+sleep 30
 
 # prepare the db to work with
-mysql -h 127.0.0.1 -utimoti -penter1 < /soft/car_ads_scrapper/car_ads_db/DDL/create_objects.sql
+sudo mysql -h 127.0.0.1 -utimoti -penter1 < /soft/car_ads_scrapper/car_ads_db/DDL/create_objects.sql
 
 
 cd /soft/car_ads_scrapper
 nohup python3 cards_finder_cars_com.py &
+
+sleep 15
+
+nohup python3 cards_scrapper_cars_com.py &
 
 
