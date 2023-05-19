cf6cb37d92976dff64094529591a0b51eb7270f9
timoti1 <timoti@tut.by>
2023-05-19 16:10:18 +0300

    fixed deployment scripts

--- deployment/setup_e2_micro_vm.sh
+++ deployment/setup_e2_micro_vm.sh
@@ -6,7 +6,7 @@
 
 if [ ! -d /mnt/disk-for-data ]; then
     echo "------------------------------------------------------------"
-    echo $(date "%X UTC:   ") "Mounting external data disk" $(echo )
+    echo $(date "%X UTC:   ") "Mounting external data disk"
     echo
     sudo mkdir -p /mnt/disk-for-data
     echo UUID=`sudo blkid -s UUID -o value /dev/sdb` /mnt/disk-for-data/ ext4 discard,defaults,nofail 0 2 | sudo tee -a /etc/fstab
@@ -17,6 +17,7 @@ if [ ! -d /mnt/disk-for-data ]; then
 
     echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Creating data folders"
+    echo
     sudo mkdir -p /mnt/disk-for-data/mysql
     sudo mkdir -p /mnt/disk-for-data/car_ads_scrapper
     echo 
@@ -24,6 +25,7 @@ if [ ! -d /mnt/disk-for-data ]; then
 
     echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Installing docker subsystem"
+    echo
     sudo apt update
     sudo apt install --yes docker.io
     echo 
@@ -31,18 +33,21 @@ if [ ! -d /mnt/disk-for-data ]; then
 
     echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Pulling mysql-server:8.0 docker image"
+    echo
     sudo docker image pull mysql/mysql-server:8.0
     echo 
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Staarting mysql-server:8.0 docker container"
+    echo
     sudo docker run --name mysql --restart=always -p 3306:3306 -v /mnt/disk-for-data/mysql:/var/lib/mysql/ -d -e "MYSQL_ROOT_PASSWORD=enter1" mysql/mysql-server:8.0
     echo 
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Cloning github repository"
+    echo
     sudo mkdir /soft
     sudo git clone https://github.com/timoti1/car_ads_scrapper /soft/car_ads_scrapper
     echo 
@@ -54,6 +59,7 @@ if [ ! -d /mnt/disk-for-data ]; then
 
     echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Installing python dependencies"
+    echo
     sudo apt install --yes python3-pip
     sudo pip3 install -r /soft/car_ads_scrapper/requirements_scrapper.txt
     echo 
@@ -62,18 +68,21 @@ if [ ! -d /mnt/disk-for-data ]; then
     # set the rdbms up
     echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Creating database (mysql) objects"
+    echo
     sudo docker exec -i mysql mysql -uroot -penter1  < /soft/car_ads_scrapper/car_ads_db/DDL/create_objects.sql
     echo 
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Creating database (mysql) users"
+    echo
     sudo docker exec -i mysql mysql -uroot -penter1  < /soft/car_ads_scrapper/mysql_setup_users.sql
     echo 
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Updating database (mysqld) settings"
+    echo
     sudo docker exec -i mysql /bash -c "cat > /etc/my.cnf" < /soft/car_ads_scrapper/deployment/mysql.cnf
     echo 
     echo
@@ -82,50 +91,57 @@ if [ ! -d /mnt/disk-for-data ]; then
 
     echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Restaring rdbms (mysql) docker container"
+    echo
     sudo docker restart mysql
     echo 
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Installing mysql client tool"
+    echo
     sudo apt install --yes mysql-client
-    echo "------------------------------------------------------------"
+    echo
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Creating a swap file (2GB)"
+    echo
     sudo mkdir -v /var/cache/swap
     cd /var/cache/swap
     sudo dd if=/dev/zero of=swapfile bs=1K count=2M
 
+    echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "Enabling swapping"
+    echo
     sudo chmod 600 swapfile
     sudo mkswap swapfile
     sudo swapon swapfile
 
-    echo $(date "%X UTC:   ") "Adding record to /etc/fstab for automounting swap file"
-    echo "/var/cache/swap/swapfile none swap sw 0 0" | sudo tee -a /etc/fstab
     echo "------------------------------------------------------------"
+    echo $(date "%X UTC:   ") "Adding record to /etc/fstab for automounting swap file"
+    echo
+    echo "/var/cache/swap/swapfile none swap sw 0 0" | sudo tee -a /etc/fstab
     echo
 else
-    echo 
+    echo "------------------------------------------------------------"
     echo $(date "%X UTC:   ") "The automation script had been executed previously"
     echo 
     echo
 fi
 
-echo 
+echo "------------------------------------------------------------"
 echo $(date "%X UTC:   ") "Waiting for everything to be started and mounted"
+echo
+echo
 # hope 1m is enough...
 sleep 60
-echo "------------------------------------------------------------"
-echo
 
 echo "------------------------------------------------------------"
 echo $(date "%X UTC:   ") "Starting cards_finder_cars_com.py"
+echo
 cd /soft/car_ads_scrapper
 sudo nohup python3 cards_finder_cars_com.py &
-echo "------------------------------------------------------------"
+echo
 echo
 
 # sleep 30
