9ac4c169d3866c7e42bd8e958165b73d5776d069
timoti1 <timoti@tut.by>
2023-05-19 16:00:19 +0300

    fixed deployment scripts

--- deployment/setup_e2_micro_vm.sh
+++ deployment/setup_e2_micro_vm.sh
@@ -1,48 +1,50 @@
 
 # sudo google_metadata_script_runner startup
 
+# in case we haven't used that external disk previously, we need to format it
 # sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
-if [! -d /mnt/disk-for-data ]; then
+
+if [ ! -d /mnt/disk-for-data ]; then
     echo "------------------------------------------------------------"
     echo $(date "+%A %W %Y, %X UTC:   ") "Mounting external data disk"
     sudo mkdir -p /mnt/disk-for-data
     echo UUID=`sudo blkid -s UUID -o value /dev/sdb` /mnt/disk-for-data/ ext4 discard,defaults,nofail 0 2 | sudo tee -a /etc/fstab
     sudo mount -o discard,defaults /dev/sdb /mnt/disk-for-data
     sudo chmod a+w /mnt/disk-for-data/
-    echo "--------------------------------------------------------"
+    echo 
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "+%A %W %Y, %X UTC:   ") "Creating data folders"
     sudo mkdir -p /mnt/disk-for-data/mysql
     sudo mkdir -p /mnt/disk-for-data/car_ads_scrapper
-    echo "--------------------------------------------------------"
+    echo 
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "+%A %W %Y, %X UTC:   ") "Installing docker subsystem"
     sudo apt update
     sudo apt install --yes docker.io
-    echo "--------------------------------------------------------"
+    echo 
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "+%A %W %Y, %X UTC:   ") "Pulling mysql-server:8.0 docker image"
     sudo docker image pull mysql/mysql-server:8.0
-    echo "--------------------------------------------------------"
+    echo 
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "+%A %W %Y, %X UTC:   ") "Staarting mysql-server:8.0 docker container"
     sudo docker run --name mysql --restart=always -p 3306:3306 -v /mnt/disk-for-data/mysql:/var/lib/mysql/ -d -e "MYSQL_ROOT_PASSWORD=enter1" mysql/mysql-server:8.0
-    echo "--------------------------------------------------------"
+    echo 
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "+%A %W %Y, %X UTC:   ") "Cloning github repository"
     sudo mkdir /soft
     sudo git clone https://github.com/timoti1/car_ads_scrapper /soft/car_ads_scrapper
-    echo "--------------------------------------------------------"
+    echo 
     echo
 
     #sudo apt install --yes openjdk-8-jre-headless
@@ -53,26 +55,26 @@ if [! -d /mnt/disk-for-data ]; then
     echo $(date "+%A %W %Y, %X UTC:   ") "Installing python dependencies"
     sudo apt install --yes python3-pip
     sudo pip3 install -r /soft/car_ads_scrapper/requirements_scrapper.txt
-    echo "--------------------------------------------------------"
+    echo 
     echo
 
     # set the rdbms up
     echo "------------------------------------------------------------"
     echo $(date "+%A %W %Y, %X UTC:   ") "Creating database (mysql) objects"
     sudo docker exec -i mysql mysql -uroot -penter1  < /soft/car_ads_scrapper/car_ads_db/DDL/create_objects.sql
-    echo "--------------------------------------------------------"
+    echo 
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "+%A %W %Y, %X UTC:   ") "Creating database (mysql) users"
     sudo docker exec -i mysql mysql -uroot -penter1  < /soft/car_ads_scrapper/mysql_setup_users.sql
-    echo "--------------------------------------------------------"
+    echo 
     echo
 
     echo "------------------------------------------------------------"
     echo $(date "+%A %W %Y, %X UTC:   ") "Updating database (mysqld) settings"
     sudo docker exec -i mysql /bash -c "cat > /etc/my.cnf" < /soft/car_ads_scrapper/deployment/mysql.cnf
-    echo "--------------------------------------------------------"
+    echo 
     echo
 
     # mysql --user=root --password="$(cat /root/.mysql)"
@@ -80,7 +82,7 @@ if [! -d /mnt/disk-for-data ]; then
     echo "------------------------------------------------------------"
     echo $(date "+%A %W %Y, %X UTC:   ") "Restaring rdbms (mysql) docker container"
     sudo docker restart mysql
-    echo "--------------------------------------------------------"
+    echo 
     echo
 
     echo "------------------------------------------------------------"
@@ -105,13 +107,13 @@ if [! -d /mnt/disk-for-data ]; then
     echo "------------------------------------------------------------"
     echo
 else
-    echo "--------------------------------------------------------"
+    echo 
     echo $(date "+%A %W %Y, %X UTC:   ") "The automation script had been executed previously"
-    echo "--------------------------------------------------------"
+    echo 
     echo
 fi
 
-echo "--------------------------------------------------------"
+echo 
 echo $(date "+%A %W %Y, %X UTC:   ") "Waiting for everything to be started and mounted"
 # hope 1m is enough...
 sleep 60
